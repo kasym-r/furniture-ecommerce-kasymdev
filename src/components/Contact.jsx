d9863ce5c76a28ab1bf8c5d432dca376004bbbba
kasym-r <kasymrakhmatyllo@gmail.com>
2024-11-10 21:07:51 +0600

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,17 +7,17 @@ const Contact = () => {
 
             <div className="container px-5 py-12 mx-auto flex sm:flex-nowrap flex-wrap">
               <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                <iframe width="100%" height="100%" className="contactMap absolute inset-0" title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193248.1770024609!2d111.5077883534262!3d40.816919186617675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3606462a4a60d3e1%3A0x7f72c427fe44f504!2sHohhot%2C%20Mongolie-int%C3%A9rieure%2C%20Chine!5e0!3m2!1sfr!2sus!4v1643714744806!5m2!1sfr!2sus"></iframe>
+              <iframe width="100%" height="100%" className="contactMap absolute inset-0" title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2925.4190179071975!2d74.60737407693176!3d42.84288480446038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb622c6122a75%3A0xa04e9fa1397bac61!2z0YPQuy4g0JDRhdGD0L3QsdCw0LXQstCw!5e0!3m2!1sru!2skg!4v1731251132934!5m2!1sru!2skg"></iframe>
                 <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
                   <div className="lg:w-1/2 px-6">
                     <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-                    <p className="mt-1">wanming street, Saihan, Hohhot, China</p>
+                    <p className="mt-1">Akhunbaev Street, Bishkek, Kyrgyzstan</p>
                   </div>
                   <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                     <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-                    <a className="text-indigo-500 leading-relaxed"> contact@sofa.com</a>
+                    <a className="text-indigo-500 leading-relaxed">sofashopbishkek@gmail.com</a>
                     <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-                    <p className="leading-relaxed">+15592810818</p>
+                    <p className="leading-relaxed">+996999999999</p>
                   </div>
                 </div>
               </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <div className=' flex flex-col justify-center  items-center md:mr-8 mb-8'>
         {/* <Link to="/" className='text-5xl select-none text-gray-900 font-bold tracking-widest'>Sofa.</Link> */}
         <img src="assets/android-chrome-512x512.png" height="80px" width="80px" alt="sofa logo"/>
-        <p className="text-gray-900 mb-2 mt-8 text-lg"><i className="mr-4 fas fa-map-marker"></i>wanming street, Saihan, Hohhot, China </p>
-        <p className="text-gray-900 mb-2 text-lg"><i className="mr-4 fas fa-at"></i> contact@sofa.com</p>
-        <p className="text-gray-900 text-lg"><i className="mr-4 fas fa-phone"></i> +15592810818</p>
+        <p className="text-gray-900 mb-2 mt-8 text-lg"><i className="mr-4 fas fa-map-marker"></i>Akhunbaev Street, Bishkek, Kyrgyzstan</p>
+        <p className="text-gray-900 mb-2 text-lg"><i className="mr-4 fas fa-at"></i>sofashopbishkek@gmail.com</p>
+        <p className="text-gray-900 text-lg"><i className="mr-4 fas fa-phone"></i> +996999999999</p>
     </div>
     <div className=' flex gap-4 lg:px-12 ml-2 flex-1 justify-evenly lg:justify-between items-center'>
             <ul className=' text-gray-900 lg:ml-20 xl:ml-48 flex flex-col items-center justify-between gap-8'>
@@ -30,7 +30,7 @@ const Footer = () => {
                     <Link to="/policy">Policy           </Link>
                 </li>
                 <li className='link'>
-                    <Link to="/shipping">shipping  </Link>
+                    <Link to="/shipping">Shipping  </Link>
                 </li>
             </ul>
     </div>
